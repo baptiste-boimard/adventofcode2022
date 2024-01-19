aea34b3040eaf8479e20aa958a7b4128858e61f7
punky <bouketin27@gmail.com>
2024-01-19 11:41:24 +0100

    Day1.1 OK

--- Day1/Elfe.cs
+++ Day1/Elfe.cs
@@ -1,12 +1,13 @@
 using System;
 using System.IO;
-
+using System.Collections.Generic;
 public class Elfe
 {
     static void Main()
     {
+        
         // récupère le fichier texte
-        string inputPath = "/home/baptiste/RiderProjects/AdventofCode/Day1/input.txt";
+        string inputPath = "/home/punky/Code/Projets/adventofcode2022/Day1/input.txt";
         // pas trouvé le moyen de le mettre en relatif
         
         //Je crée un liste vide qui va récupérer chaque ligne du fichier text
@@ -23,10 +24,9 @@ public class Elfe
 
                     // lit une ligne du fichier
                     string lign = streamReader.ReadLine();
-
+                    
+                    // Ajout de la ligne dans la liste
                     listInput.Add(lign);
-
-                    //Console.WriteLine(lign);
                 }
             }
         }
@@ -34,16 +34,49 @@ public class Elfe
         {
             Console.WriteLine("Le fichier est introuvable");
         }
+        
         // Une liste de pas être obtenu en console sans un foreach ?
+        
         //Console.WriteLine(listInput);
-        foreach (var input in listInput)
+        // foreach (var input in listInput)
+        // {
+        //     Console.WriteLine(input);
+        // }
+        // Console.WriteLine(listInput.Count);
+        
+        // Autre manière de le faire
+        listInput.ForEach(i => Console.Write("{0}\t", i));
+        
+        // Créer une variable qui stocke temporairement le total pour chaque elfe
+        int totalElfe = 0;
+        
+        // Créer une liste qui contient le total pour chaque elfe
+        List<int> listTotalElfe = new List<int>();
+
+        for (int i = 0; i < listInput.Count; i++)
         {
-            Console.WriteLine(input);
+            
+            if (string.IsNullOrWhiteSpace(listInput[i]))
+            {
+                Console.WriteLine(listInput[i]);
+                listTotalElfe.Add(totalElfe);
+                Console.WriteLine("total section" + totalElfe);
+                totalElfe = 0;
+            }
+            else
+            {
+                Console.WriteLine(listInput[i]);
+                int listInputIntConvert = int.Parse(listInput[i]);
+                totalElfe += listInputIntConvert;
+                // Console.WriteLine("total partiel" + totalElfe);
+            }
         }
-        Console.WriteLine(listInput.Count);
+        // On recherche la valeur max de la liste des total pour chaque elfe
+        int maxValueForOneElfe = listTotalElfe.Max();
         
-        
-        
-        
+        Console.WriteLine("Le max est " + maxValueForOneElfe);
+
+
+
     }
 }
--- Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net8.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1162faaaf4f228a04f189f82215223c1a51632d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ea53b94a53278f88dc99f0bebc3f8475deb730")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
