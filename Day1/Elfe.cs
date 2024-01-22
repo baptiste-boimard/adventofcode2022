4a3e73bee5c2a33cb2dfda8cd5c8f327381606ab
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2024-01-22 09:53:42 +0100

    Factorisation du jour 1

--- Day1/Elfe.cs
+++ Day1/Elfe.cs
@@ -1,39 +1,23 @@
-using System;
-using System.IO;
-using System.Collections.Generic;
-public class Elfe
+namespace Day1;
+
+public class Program
 {
     static void Main()
     {
         
         // récupère le fichier texte
-        string inputPath = "/home/punky/Code/Projets/adventofcode2022/Day1/input.txt";
+        string inputPath = "../../../input.txt";
         // pas trouvé le moyen de le mettre en relatif
         
-        //Je crée un liste vide qui va récupérer chaque ligne du fichier text
-        List<string> listInput = new List<string>();
-        
         // Vérifie si le fichier existe
-        if (File.Exists(inputPath))
-        {
-            using (StreamReader streamReader = new StreamReader(inputPath))
-            {
-                // lit chaque ligne du fichier jusqu'à la fin
-                while (!streamReader.EndOfStream)
-                {
-
-                    // lit une ligne du fichier
-                    string lign = streamReader.ReadLine();
-                    
-                    // Ajout de la ligne dans la liste
-                    listInput.Add(lign);
-                }
-            }
-        }
-        else
+        if (!File.Exists(inputPath))
         {
             Console.WriteLine("Le fichier est introuvable");
+            return;
         }
+
+        //Je crée un liste vide qui va récupérer chaque ligne du fichier text
+        string[] listInput = File.ReadAllText(inputPath).Split("\r\n").ToArray();
         
         // Une liste de pas être obtenu en console sans un foreach ?
         
@@ -53,10 +37,9 @@ public class Elfe
         // Créer une liste qui contient le total pour chaque elfe
         List<int> listTotalElfe = new List<int>();
 
-        for (int i = 0; i < listInput.Count; i++)
+        foreach (string s in listInput)
         {
-            
-            if (string.IsNullOrWhiteSpace(listInput[i]))
+            if (string.IsNullOrWhiteSpace(s))
             {
                 // Console.WriteLine(listInput[i]);
                 listTotalElfe.Add(totalElfe);
@@ -66,33 +49,33 @@ public class Elfe
             else
             {
                 // Console.WriteLine(listInput[i]);
-                int listInputIntConvert = int.Parse(listInput[i]);
+                int listInputIntConvert = int.Parse(s);
                 totalElfe += listInputIntConvert;
                 // Console.WriteLine("total partiel" + totalElfe);
             }
         }
-        // On recherche la valeur max de la liste des total pour chaque elfe
-        int maxValueForOneElfe = listTotalElfe.Max();
-        
-        Console.WriteLine("Le max transporté par un elfe est " + maxValueForOneElfe);
-        
+       
         // Créée variable qui totalise la somme des calories des 3 + gros porteurs
-        int sommeDes3PlusGrosElfe = 0;
+        //int sommeDes3PlusGrosElfe = 0;
 
         //Itération sur la liste des totaux pour trouver le max
         // on l'ajoute à la somme puis on le delete de la liste
         // on refait l'opération 2 fois pour avoir les 3 1er
-        for (int j = 0; j < 3; j++)
-        {
-            int maxValueOfList = listTotalElfe.Max();
-            sommeDes3PlusGrosElfe += maxValueOfList;
-            listTotalElfe.Remove(maxValueOfList);
-            
-            Console.WriteLine($"Elfe N°{j+1} => total = {sommeDes3PlusGrosElfe}");
-            
-        }
-        
-        Console.WriteLine("Les Calories portées par les 3 + gros sont de : " + sommeDes3PlusGrosElfe);
+        /*for (int j = 0; j < 3; j++)
+    {
+        int maxValueOfList = listTotalElfe.Max();
+        sommeDes3PlusGrosElfe += maxValueOfList;
+        listTotalElfe.Remove(maxValueOfList);
+
+        Console.WriteLine($"Elfe N°{j+1} => total = {sommeDes3PlusGrosElfe}");
+
+    }*/
+
+        int[] found = listTotalElfe.OrderByDescending(i => i)
+            .Take(3)
+            .ToArray();
+
+        Console.WriteLine("Les Calories portées par les 3 + gros sont de : " + found.Sum());
 
     }
 }
